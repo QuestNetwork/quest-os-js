c84a07bdd5db90334ea659f954d9f841af8b9277
StationedInTheField <ask@stationedinthefield.com>
2020-09-08 04:41:18 -0700

    os refactor

--- os.js
+++ os.js
@@ -2,6 +2,7 @@ import * as Ipfs from 'ipfs';
 const { v4: uuidv4 } = require('uuid');
 import { Subject } from "rxjs";
 import { Ocean }  from '@questnetwork/quest-ocean-js';
+import { BeeSwarmInstance }  from '@questnetwork/quest-bee-js';
 
 export class OperatingSystem {
     constructor() {
@@ -9,6 +10,7 @@ export class OperatingSystem {
       this.ocean = Ocean;
       this.ready = false;
       this.isReadySub = new Subject();
+      this.bee = new BeeSwarmInstance();
     }
 
     delay(t, val = "") {
@@ -21,6 +23,22 @@ export class OperatingSystem {
 
     async boot(config){
       await this.ocean.create(config);
+
+      this.bee.start({ electronService: config['dependencies']['electronService'], version: config['version']);
+
+      this.ocean.dolphin.commitNowSub.subscribe( (value) => {
+        this.bee.config.commitNow();
+      });
+
+      this.ocean.dolphin.commitSub.subscribe( (value) => {
+        this.bee.config.commit();
+      });
+
+
+      this.ocean.dolphin.selectedChannelSub.subscribe( (value) => {
+        this.bee.config.selectChannel(value);
+      });
+
       this.ready = true;
       this.isReadySub.next(true);
       return true;
@@ -29,6 +47,34 @@ export class OperatingSystem {
     isReady(){
       return this.ready;
     }
+
+
+
+
+
+   async createChannel(channelNameDirty, parentFolderId = ""){
+     let channelNameClean = await this.ocean.dolphin.createChannel(channelNameDirty);
+     this.bee.config.addToChannelFolderList(channelNameClean, parentFolderId);
+     return channelNameClean;
+   }
+   async addChannel(channelNameClean, parentFolderId = ""){
+     try{
+       await this.dolphin.addChannel(channelNameClean);
+     }catch(e){}
+     this.bee.config.addToChannelFolderList(channelNameClean, parentFolderId);
+     return channelNameClean;
+   }
+   removeChannel(channel){
+     //remove from channelNameList
+     let channelNameList = this.ocean.dolphin.getChannelNameList().filter(e => e != channel);
+     this.ocean.dolphin.setChannelNameList(channelNameList);
+     //remove from channelFolderList
+     let chfl = this.bee.config.getChannelFolderList();
+     chfl = this.bee.config.parseFolderStructureAndRemoveItem(chfl, channel);
+     this.bee.config.setChannelFolderList(chfl);
+   }
+
+
 
 
   }
