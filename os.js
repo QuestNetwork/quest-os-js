2964fab0c38b0885f005018b8887a38a51a24bfc
StationedInTheField <ask@stationedinthefield.com>
2020-09-10 09:28:00 -0700

    autosave

--- os.js
+++ os.js
@@ -21,6 +21,10 @@ export class OperatingSystem {
        });
     }
 
+    isSignedIn(){
+      return this.bee.config.isSignedIn();
+    }
+
     async boot(config){
       await this.ocean.create(config);
       config['dependencies']['dolphin'] = this.ocean.dolphin;
@@ -48,60 +52,42 @@ export class OperatingSystem {
       return this.ready;
     }
 
+    commit(){
+      this.bee.config.commit();
+    }
     commitNow(){
       this.bee.config.commitNow();
     }
 
 
     async importChannel(channelName,folders,parentFolderId,inviteToken,importFolderStructure){
-
+      if(typeof parentFolderId == 'undefined'){parentFolderId = ""}
       if(importFolderStructure == 1 && folders.length > 0){
           //see if folders exist starting at parentFolderId
           console.log(parentFolderId);
           let chfl = this.bee.config.getChannelFolderList();
           for(let i=0; i<folders.length;i++){
-            let newFolder = { data: { name: folders[i], kind:"dir", items: 0 }, id: uuidv4(),expanded: true, children: [] };
-
-            if(parentFolderId == ""){
-              //check if exist at top level
-              let exists = false;
-              for (let i2=0;i2<chfl.length;i2++){
-                if(chfl[i2]['data']['name'] == newFolder['data']['name']){
-                  exists = true;
-                  if(typeof chfl[i2]['id'] == 'undefined'){
-                    chfl[i2]['id'] = uuidv4();
-                  }
-                  parentFolderId = chfl[i2]['id'];
-                  this.pFICache = parentFolderId;
-                }
-              }
-              if(!exists){
-                parentFolderId = newFolder['id'];
-                this.pFICache = parentFolderId;
-                chfl.push(newFolder);
-              }
-            }
-            else{
-              chfl = this.bee.config.parseFolderStructureAndPushItem(chfl, parentFolderId, newFolder, true);
-              if(typeof this.pFICache != 'undefined' && this.pFICache != null){
-                parentFolderId = this.pFICache;
-              }
-           }
-
-
+            let newFolder = { id: uuidv4(), data: { name: folders[i], kind:"dir", items: 0 }, expanded: true, children: [] };
+            chfl = this.bee.config.parseFolderStructureAndPushItem(chfl, parentFolderId, newFolder, true);
+            parentFolderId = this.bee.config.getParseAndImportParentIdCache();
           }
-          this.pFICache = null;
-         this.bee.config.setChannelFolderList(chfl);
+
+          this.bee.config.setChannelFolderList(chfl);
+          await this.addChannel(channelName, parentFolderId);
+          this.ocean.dolphin.addInviteToken(channelName,inviteToken);
+      }
+      else{
+        console.log(parentFolderId);
+        await this.addChannel(channelName, parentFolderId);
+        this.ocean.dolphin.addInviteToken(channelName,inviteToken);
       }
 
-      console.log(parentFolderId);
-      await this.addChannel(channelName, parentFolderId);
-      this.ocean.dolphin.addInviteToken(channelName,inviteToken);
       return true;
     }
    async createChannel(channelNameDirty, parentFolderId = ""){
      let channelNameClean = await this.ocean.dolphin.createChannel(channelNameDirty);
      this.bee.config.addToChannelFolderList(channelNameClean, parentFolderId);
+     this.commitNow();
      return channelNameClean;
    }
    async addChannel(channelNameClean, parentFolderId = ""){
@@ -109,6 +95,7 @@ export class OperatingSystem {
        await this.ocean.dolphin.addChannel(channelNameClean);
      }catch(e){}
      this.bee.config.addToChannelFolderList(channelNameClean, parentFolderId);
+     this.commitNow();
      return channelNameClean;
    }
    removeChannel(channel){
@@ -119,7 +106,7 @@ export class OperatingSystem {
      let chfl = this.bee.config.getChannelFolderList();
      chfl = this.bee.config.parseFolderStructureAndRemoveItem(chfl, channel);
      this.bee.config.setChannelFolderList(chfl);
-
+     this.commitNow();
    }
    createInvite(channel,newInviteCodeMax, importFolders = false){
      let code = uuidv4();
