00de7def5fc95afd32ca99722966874b2e36e378
StationedInTheField <ask@stationedinthefield.com>
2020-10-01 14:02:40 -0700

    request prep

--- request/requestManager.js
+++ request/requestManager.js
@@ -5,6 +5,10 @@ const { v4: uuidv4 } = require('uuid');
 
 export class RequestManager {
 
+  constructor(){
+    this.listenSub = {};
+  }
+
   load(config){
     this.dolphin = config['dependencies']['dolphin'];
     this.channel = config['dependencies']['channel'];
@@ -22,7 +26,7 @@ export class RequestManager {
         let postSub = this.channel.listen(object['channel']);
         postSub.subscribe( (responseObject) => {
           //if this is the response to out request , do action.....
-          if(responseObject['tyoe'] == "RESPONSE" && responseObject['path'] == postObj['path']){
+          if(responseObject['type'] == "RESPONSE" && responseObject['path'] == postObj['path']){
             //unsubscribe and resolve
             postSub.unsubscribe();
             resolve(responseObject);
@@ -31,6 +35,8 @@ export class RequestManager {
 
         //publish our request object
         postObj['type'] = "REQUEST";
+        postObj['channel'] = object['channel'];
+
         this.channel.publish(postObj);
       }
       //close connection after timeeout
@@ -42,14 +48,31 @@ export class RequestManager {
     });
   }
 
-  listen(path){
-    //forward pubsub subject with valid requests for path
-      //TO DO
+
+  listenWorker(channel){
+    this.channel.listen(channel).subscribe((request) => {
+      if(request['type'] == "REQUEST" && request['path'] == path){
+        //unsubscribe and resolve
+        listenSub.next(request)
+      }
+    }
   }
 
-  res(respondObj){
-    //post response object to channel
-      //TO DO
+  listen(path){
+    let channelNameList = await this.dolphin.getChannelNameList();
+    for(let channel of channelNameList){
+      listenSub[path] = this.listenWorker(channel);
+    }
+    return listenSub[path];
+  }
+
+  async res(respondObj){
+    respondObj['type'] = "RESPONSE";
+    let channelNameList = await this.dolphin.getChannelNameList();
+    for(let channel of channelNameList){
+      respondObj['channel'] = channel;
+      this.channel.publish(respondObj);
+    }
     return true;
   }
 
